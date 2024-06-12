80da7f5244a4841b6a9b9d543430f515a0f0e2ef
Steven Hutchison <hutchison.music@gmail.com>
2024-06-12 17:03:00 -0500

    Add Share to Google Classroom link to success page with quiz info from api call

--- frontend/src/Pages/Success.tsx
+++ frontend/src/Pages/Success.tsx
@@ -1,10 +1,30 @@
 import { useNavigate, useParams } from "react-router-dom"
 import PageNotFound from "./PageNotFound";
+import classroomIcon from '../assets/classroom.png'
 import './Success.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Success() {
     const {quizId} = useParams();
     const navigate = useNavigate();
+    const quizLink = `https://www.mediaquizlet.com/quizzer/${quizId}`;
+    const [quizInfo, setQuizInfo] = useState<{title: string, description: string}>({title: 'Media Quizlet', description: 'Your teacher created a quiz for you on Media Quizlet!'});
+
+    useEffect(()=>{
+        if (quizId && quizId !== 'responseSubmitted') {
+            axios.get(`https://audio-quizlet.vercel.app/api/quizzes/${quizId}`)
+                .then(res => {
+                    const {title, description} = res.data;
+                    setQuizInfo({title, description})
+                })
+                .catch(err =>{
+                    console.log(err);    
+                })
+        }
+            
+        }, [])
+
     if (!quizId) {
         return <PageNotFound />
     }
@@ -17,16 +37,24 @@ export default function Success() {
     }
 
     const copyLink = ()=>{
-        navigator.clipboard.writeText(`https://www.mediaquizlet.com/quizzer/${quizId}`)
+        navigator.clipboard.writeText(quizLink)
     }
+
+    
 
     return (
         <div className="successWrap">
             <h2>Quizlet Successfully Created</h2>
             <p>Link to Quizlet:</p>
             <div>
-                <a id="successLink" href={`https://www.mediaquizlet.com/quizzer/${quizId}`}>{`https://www.mediaquizlet.com/quizzer/${quizId}`}</a>
-                <button id="copyLink" onClick={copyLink} onKeyDown={e=>e.key==='Enter'&&copyLink()}><i className="fa-solid fa-copy"></i></button>
+                <a id="successLink" href={quizLink}>{quizLink}</a>
+                <div>
+                    
+                        <button id="copyLink" onClick={copyLink} onKeyDown={e=>e.key==='Enter'&&copyLink()}><i className="fa-solid fa-copy"></i></button>
+                        <a id='classroom' href={`https://classroom.google.com/share?url=${quizLink}&title=${encodeURIComponent(quizInfo.title)}&body=${encodeURIComponent(quizInfo.description)}`} aria-label="Add to Google Classroom">
+                            <img src={classroomIcon} width='40px' alt="Add to Google Classroom"></img>
+                        </a>
+                </div>
             </div>
             <p>Quizlet Code:</p>
             <div>
