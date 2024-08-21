10cac5b12764af033c03e4fc386a5365d7aa109e
Steven Hutchison <hutchison.music@gmail.com>
2024-08-21 07:17:25 -0500

    clean up document formatting

--- frontend/src/Routes/Router.tsx
+++ frontend/src/Routes/Router.tsx
@@ -13,50 +13,55 @@ import UserQuizzes from "../Pages/UserQuizzes";
 import PageNotFound from "../Pages/PageNotFound";
 
 export default function ReactRoutes() {
-
-    function Frame() {
-        return (
-            <>
-                <Header />
-                <Outlet />
-            </>
-        )
-    }
-
+  function Frame() {
     return (
-            <Routes>
-                <Route path="/" element={<Frame />}>
-                    <Route index element={<Home />}/>
-                    <Route path="quizzer">
-                        <Route index element={<Navigate to='/lookup'/>} />
-                        <Route path=":quizId" element={
-                            <QuizProvider>
-                                <ResponseProvider>
-                                    <Quizzer />
-                                </ResponseProvider>
-                            </QuizProvider>
-                        }/>
-                    </Route>
-                    <Route path="viewer">
-                        <Route index element={<Navigate to='/lookup'/>} />
-                        <Route path=":quizId" element={<Viewer />} />
-                    </Route> 
-                    <Route path="lookup" element={<Lookup />} />
-                    <Route path="users" >
-                        <Route index element={<Navigate to='/lookup'/>} />
-                        <Route path=":userId" element={<UserQuizzes />} />
-                    </Route>
-                    <Route path="success">
-                        <Route index element={<PageNotFound />}/>
-                        <Route path=":quizId" element={<Success />} />
-                    </Route>
-                </Route>
-                <Route path="/generator" element={
-                    <GeneratorContextProvider>
-                        <Generator />
-                    </GeneratorContextProvider>
-                }/>
-                <Route path="/*" element={<PageNotFound />} /> 
-            </Routes>
-    )
+      <>
+        <Header />
+        <Outlet />
+      </>
+    );
+  }
+
+  return (
+    <Routes>
+      <Route path="/" element={<Frame />}>
+        <Route index element={<Home />} />
+        <Route path="quizzer">
+          <Route index element={<Navigate to="/lookup" />} />
+          <Route
+            path=":quizId"
+            element={
+              <QuizProvider>
+                <ResponseProvider>
+                  <Quizzer />
+                </ResponseProvider>
+              </QuizProvider>
+            }
+          />
+        </Route>
+        <Route path="viewer">
+          <Route index element={<Navigate to="/lookup" />} />
+          <Route path=":quizId" element={<Viewer />} />
+        </Route>
+        <Route path="lookup" element={<Lookup />} />
+        <Route path="users">
+          <Route index element={<Navigate to="/lookup" />} />
+          <Route path=":userId" element={<UserQuizzes />} />
+        </Route>
+        <Route path="success">
+          <Route index element={<PageNotFound />} />
+          <Route path=":quizId" element={<Success />} />
+        </Route>
+      </Route>
+      <Route
+        path="/generator"
+        element={
+          <GeneratorContextProvider>
+            <Generator />
+          </GeneratorContextProvider>
+        }
+      />
+      <Route path="/*" element={<PageNotFound />} />
+    </Routes>
+  );
 }
